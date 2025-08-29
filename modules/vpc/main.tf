f7de6c9d406ad602ac0865dc0d8b6daa339b0d1d
squirrel <dummy>
2025-08-29 20:52:35 +0900

    add public/private route tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -29,6 +29,7 @@ resource "aws_subnet" "public" {
     }
 }
 
+# private subnets
 resource "aws_subnet" "private" {
     count             = 2
     vpc_id            = aws_vpc.this.id
@@ -40,3 +41,40 @@ resource "aws_subnet" "private" {
     }
 }
 
+# public route tables
+resource "aws_route_table" "public" {
+    count   = 2
+    vpc_id = aws_vpc.this.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+    tags = {
+        Name = "${var.name}-public-route-table-${count.index + 1}"
+    }
+}
+
+# associate public route table to public subnets
+resource "aws_route_table_association" "public" {
+    count          = 2
+    subnet_id      = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public[count.index].id
+}
+
+# private route tables
+resource "aws_route_table" "private" {
+    count   = 2
+    vpc_id = aws_vpc.this.id
+
+    tags = {
+        Name = "${var.name}-private-route-table-${count.index + 1}"
+    }
+}
+
+# associate private route table to private subnets
+resource "aws_route_table_association" "private" {
+    count          = 2
+    subnet_id      = aws_subnet.private[count.index].id
+    route_table_id = aws_route_table.private[count.index].id
+}
